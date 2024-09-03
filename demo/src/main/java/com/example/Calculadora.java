54de141dff25f32ccd9930164a99284fbea98f95
ERICK RIBEIRO DE SOUZA <erick_ribeiro-souza@estudante.sesisenai.org.br>
2024-09-02 21:42:10 -0300

    Calculadora

--- demo/src/main/java/com/example/Calculadora.java
+++ demo/src/main/java/com/example/Calculadora.java
@@ -7,6 +7,21 @@ public Integer soma(Integer v1, Integer v2){
     return v1 + v2;
 
 }
+public Integer subtracao(Integer v1, Integer v2){
+
+    return v1 - v2;
+
+}
+public Integer multplicacao(Integer v1, Integer v2){
+
+    return v1 * v2;
+
+}
+public Integer divisao(Integer v1, Integer v2){
+
+    return v1 / v2;
+
+}
 
 
 }
--- demo/src/test/java/CalculadoraTest.java
+++ demo/src/test/java/CalculadoraTest.java
@@ -11,5 +11,26 @@ public class CalculadoraTest {
         assertEquals(4, result);
 
     }
+    @Test
+    public void testesubtracao(){
+        Calculadora calc = new Calculadora();
+        Integer result = calc.subtracao(4, 3);
+        assertEquals(1, result);
+
+    }
+    @Test
+    public void testemultplicacao(){
+        Calculadora calc = new Calculadora();
+        Integer result = calc.multplicacao(5, 5);
+        assertEquals(25, result);
+
+    }
+    @Test
+    public void testedivisao(){
+        Calculadora calc = new Calculadora();
+        Integer result = calc.divisao(10, 2);
+        assertEquals(5, result);
+
+    }
     
 }
